57e7d59ee4f216f68ba3369140d455ed25057122
Gavyn-McCool <161868270+Gavyn-McCool@users.noreply.github.com>
2024-03-01 12:05:52 -0500

    Update Game_Class.java

--- Game_Class.java
+++ Game_Class.java
@@ -3,7 +3,9 @@ public class Game{
   // Game variables
   int boardSize = 3;
 
-  public static void Game(){
+  public static void Game(int size){
 
+    // Set the variables
+    boardSize = size;
   }
 }
