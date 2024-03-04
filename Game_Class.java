6f8dae44e619acfdc8aab4182ad2fdf1baf14f15
Gavyn-McCool <161868270+Gavyn-McCool@users.noreply.github.com>
2024-03-04 12:03:37 -0500

    Update Game_Class.java

--- Game_Class.java
+++ Game_Class.java
@@ -45,13 +45,13 @@ public class Game{
 
   public static boolean CheckWin(ArrayList<ArrayList<String>> board, String selection){
 
-    // Convert the selection to uppercase
-    selection = selection.toUpperCase();
-    	
-    // Make sure the selection is valid
-    if(!(selection.equals("X") || selection.equals("O"))) return false;
-    	
-    // Check for a full row
+		// Convert the selection to uppercase
+		selection = selection.toUpperCase();
+
+		// Make sure the selection is valid
+		if(!(selection.equals("X") || selection.equals("O")) return false;
+
+		// Check for a full row
     for(int i = 0; i < game.size(); i++){
     		
     	// Get the row
@@ -68,64 +68,8 @@ public class Game{
     	}
     		
     	// Check if the row is a winning pattern
-    	if(streak == row.size()){
-    		return true;
-    	}
-    }
-    	
-    // Check for a full column
-    for(int i = 0; i < game.get(0).size(); i++){
-    		
-      int streak = 0;
-    		
-    	// Loop through each column
-    	for(int v = 0; v < game.size(); v++){
-    			
-    		  // Get the column
-    			ArrayList<String> column = game.get(v);
-    			
-    			// Check if string equals the selection
-    			if(column.get(i).equals(selection)){
-    				streak++;
-    			}
-    		}
-    		
-    		// Check if the row is a winning pattern
-    		if(streak == game.size()){
-    			return true;
-    		}
-    	}
+    	if(streak == row.size()) return true;
 
-    	// Check for a right diagonal
-    	int streakR = 0;
-    	for(int i = 0; i < game.size(); i++){
-    		
-    		// Check if string equals the selection
-    		if(game.get(i).get(i).equals(selection)){
-    			streakR++;
-    		}
-    	}
-    	
-    	// Check if the diagonal is a winning pattern
-    	if(streakR == game.size()){
-    		return true;
-    	}
-
-    	// Check for a left diagonal
-    	int streakL = 0;
-    	for(int i = game.size() - 1; i >= 0; i--){
-    		
-    		// Check if string equals the selection
-    		if(game.get(i).get(i).equals(selection)){
-    			streakL++;
-    		}
-    	}
-    	
-    	// Check if the diagonal is a winning pattern
-    	if(streakL == game.size()){
-    		return true;
-    	}
-    	
-    	return false;
-  }
+		}
+	}
 }
