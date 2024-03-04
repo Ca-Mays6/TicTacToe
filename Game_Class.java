363f02965380576e6a1543c54aa6499165d830a9
Gavyn-McCool <161868270+Gavyn-McCool@users.noreply.github.com>
2024-03-04 11:54:58 -0500

    Update Game_Class.java

--- Game_Class.java
+++ Game_Class.java
@@ -17,4 +17,24 @@ public class Game{
     // The board of itself
     ArrayList<ArrayList<String>> board = new ArrayList<ArrayList<String>>();
   }
+
+  // || BOARD PLACEMENT ||
+  
+  public static boolean UpdateBoard(int row, int col, String symbol){
+
+    // Make sure it is a valid placement
+    if(!CheckValidPlacement(row, col, symbol)) return false;
+
+    // Place the symbol
+    
+    // Let the caller know the symbol was placed on the board
+    return true;
+  }
+
+  public static boolean CheckValidPlacement(int row, int col, String symbol){
+
+    return true;
+  }
+
+  // || WIN CONDITIONS ||
 }
