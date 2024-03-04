479d689990ab9cfbcec6995f585e71294195655a
Gavyn-McCool <161868270+Gavyn-McCool@users.noreply.github.com>
2024-03-04 11:51:24 -0500

    Update Game_Class.java

--- Game_Class.java
+++ Game_Class.java
@@ -1,3 +1,9 @@
+import java.io.*;
+import java.io.*;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Game{
 
   // Game variables
@@ -5,7 +11,10 @@ public class Game{
 
   public static void Game(int size){
 
-    // Set the variables
+    // The size of the board
     boardSize = size;
+
+    // The board of itself
+    ArrayList<ArrayList<String>> board = new ArrayList<ArrayList<String>>();
   }
 }
