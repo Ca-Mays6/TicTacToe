4ce6981ed68cbb218ffab04be69b09596395dbdc
Ca-Mays6 <153109765+Ca-Mays6@users.noreply.github.com>
2024-03-04 11:56:16 -0500

    Update TicTacToe.java
    beginning of frame

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,26 @@
+import javax.swing.*;
+java.awt.*;
+
+
+
 public class Main 
 {
 
+  public GUI()
+    {
+  JFrame frame = new JFrame("Game Board");
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+  
+      
+      frame.pack();
+      frame.setVisible(true);
+  }
+
+
+  
   public static void main(String[] args) 
   {
     System.out.println("Hello World");
+    new GUI();
   }
 }
