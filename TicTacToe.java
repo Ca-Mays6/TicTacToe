6a085a9a24124c75edf408db73ef573d6bff6d8c
Ca-Mays6 <153109765+Ca-Mays6@users.noreply.github.com>
2024-03-05 11:50:04 -0500

    Update TicTacToe.java
    Frame for board

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,24 +3,48 @@ java.awt.*;
 
 
 
-public class Main 
+public class TicTacToe extends JPanel
 {
 
-  public GUI()
+   public TictacToe()
     {
-  JFrame frame = new JFrame("Game Board");
-  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
-  
-      
-      frame.pack();
-      frame.setVisible(true);
-  }
+    	JFrame frame = new JFrame("TicTacToe");
+    	JPanel panel = new JPanel();
+    	JButton b1 = new JButton("");
+    	JButton b2 = new JButton("");
+    	JButton b3 = new JButton("");
+    	JButton b4 = new JButton("");
+    	JButton b5 = new JButton("");
+    	JButton b6 = new JButton("");
+    	JButton b7 = new JButton("");
+    	JButton b8 = new JButton("");
+    	JButton b9 = new JButton("");
+    	panel.setBorder(BorderFactory.createEmptyBorder());
+    	panel.setLayout(new GridLayout(3,3));
+    	panel.add(b1);
+    	panel.add(b2);
+    	panel.add(b3);
+    	panel.add(b4);
+    	panel.add(b5);
+    	panel.add(b6);
+    	panel.add(b7);
+    	panel.add(b8);
+    	panel.add(b9);
+    	frame.add(panel, BorderLayout.CENTER);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
+   
+    	
+    	frame.pack();
+    	frame.setVisible(true);
+    }
+    
 
 
   
   public static void main(String[] args) 
   {
-    System.out.println("Hello World");
-    new GUI();
+
+    new TicTacToe();
   }
 }
